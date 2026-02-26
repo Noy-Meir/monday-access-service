6cb8faaaaafcb887645ebaef8e1c143a8bd8f0bc
Noy-Meir <noy2432@gmail.com>
2026-02-26 13:49:27 +0200

    change cors url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3001',
   credentials: true
 }));
 
